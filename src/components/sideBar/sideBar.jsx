5e31c40bb972dd847b289fd1f70c173cda03b2ac
Ines Cunha <inescunha@student.dei.uc.pt>
2024-04-03 19:55:38 +0100

    botoes do sidebar

--- src/components/sideBar/sideBar.jsx
+++ src/components/sideBar/sideBar.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
-import { FaHome, FaUsers, FaUser } from 'react-icons/fa';
+import { FaHome, FaUsers, FaUser, FaPlus, FaTrash } from 'react-icons/fa';
 import './sideBar.css';
+import { useLocation } from 'react-router-dom';
 
 function Sidebar() {
-  return (
-    <Nav defaultActiveKey="/home" className="flex-column sidebar">
-      <Nav.Link href="/home"><FaHome /> Home</Nav.Link>
-      <Nav.Link href="/users"><FaUsers /> Users</Nav.Link>
-      <Nav.Link href="/profile"><FaUser /> Profile</Nav.Link>
-      {/* Adicione mais links conforme necessário */}
-    </Nav>
-  );
-}
-
-export default Sidebar;
+    const location = useLocation();
+    const showButtons = location.pathname === '/home';
+  
+    return (
+        <Nav defaultActiveKey="/home" className="flex-column sidebar">
+          <Nav.Link className="sidebar-mainlink" href="/home"><FaHome /> Home</Nav.Link>
+          {showButtons && (
+            <>
+              <Nav.Link className="sidebar-sublink" href="/new-task"><FaPlus /> New Task</Nav.Link>
+              <Nav.Link className="sidebar-sublink" href="/deleted-tasks"><FaTrash /> Deleted Tasks</Nav.Link>
+            </>
+          )}
+          <Nav.Link className="sidebar-mainlink" href="/users"><FaUsers /> Users</Nav.Link>
+          <Nav.Link className="sidebar-mainlink" href="/profile"><FaUser /> Profile</Nav.Link>
+        </Nav>
+      );
+    }
+    
+    export default Sidebar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,25 +3,14 @@ import { useLocation } from 'react-router-dom';
 import Sidebar from '../components/sideBar/sideBar';
 
 function Home() {
-  const location = useLocation();
-  const [showButtons, setShowButtons] = useState(false);
-
-  useEffect(() => {
-    // Atualiza o estado showButtons quando a rota muda
-    setShowButtons(location.pathname === '/home');
-  }, [location]);
-
-  return (
-    <div>
-      <Sidebar />
-      {showButtons && (
-        <div>
-          <button>New Task</button>
-          <button>Deleted Tasks</button>
-        </div>
-      )}
-    </div>
-  );
-}
+    const location = useLocation();
+    const [showButtons, setShowButtons] = useState(false);
+  
+    return (
+      <div>
+        <Sidebar />
+      </div>
+    );
+  }
 
 export default Home;
